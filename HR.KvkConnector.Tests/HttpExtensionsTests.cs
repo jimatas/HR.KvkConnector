b5467a0923f21d3ad88bba9e6fa2c532c1f5be44
jimatas <atask@hr.nl>
2022-03-07 12:36:13 +0100

    Added unit and integration tests.

---
+++ HR.KvkConnector.Tests/HttpExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿using HR.KvkConnector.Infrastructure;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Net;
+
+namespace HR.KvkConnector.Tests
+{
+    [TestClass]
+    public class HttpExtensionsTests
+    {
+        [TestMethod]
+        public void AddTrustedRootCertificates_ByDefault_SetsServerCertificateValidationCallback()
+        {
+            // Arrange
+            var httpRequest = WebRequest.CreateHttp("https://ssltest.kvk.nl");
+
+            // Act
+            var callbackBefore = httpRequest.ServerCertificateValidationCallback;
+            httpRequest.AddTrustedRootCertificates();
+            var callbackAfter = httpRequest.ServerCertificateValidationCallback;
+
+            // Assert
+            Assert.IsNull(callbackBefore);
+            Assert.IsNotNull(callbackAfter);
+        }
+
+        #region Integration tests
+        [TestMethod]
+        public void ExecutingWebRequest_WithoutCallingAddTrustedRootCertificates_ThrowsException()
+        {
+            // Arrange
+            var httpRequest = WebRequest.CreateHttp("https://ssltest.kvk.nl");
+
+            // Act
+            void action() => httpRequest.GetResponse();
+
+            // Assert
+            Assert.ThrowsException<WebException>(action);
+        }
+
+        [TestMethod]
+        public void ExecutingWebRequest_AfterCallingAddTrustedRootCertificates_ReturnsResponse()
+        {
+            // Arrange
+            var httpRequest = WebRequest.CreateHttp("https://ssltest.kvk.nl");
+            httpRequest.AddTrustedRootCertificates();
+
+            // Act
+            using var httpResponse = httpRequest.GetResponse();
+
+            // Assert
+            Assert.IsNotNull(httpResponse);
+        }
+        #endregion
+    }
+}
