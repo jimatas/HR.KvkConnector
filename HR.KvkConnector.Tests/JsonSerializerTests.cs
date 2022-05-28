f3f7edc6b65acf385de73c7c1c918cb17ef4ef3b
jimatas <atask@hr.nl>
2022-05-28 15:22:03 +0200

    Standardized the fix for parsing invalid date/times using a shadow property by making it more consistent with other such properties (protected access, get/set symmetry). Removed some unavailable references from project that prevented it from compiling. Bumped patch version.

--- HR.KvkConnector.Tests/JsonSerializerTests.cs
+++ HR.KvkConnector.Tests/JsonSerializerTests.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System;
 using System.Runtime.Serialization;
 
 namespace HR.KvkConnector.Tests
@@ -74,6 +75,36 @@ namespace HR.KvkConnector.Tests
 
             // Assert
             Assert.ThrowsException<SerializationException>(action);
+        }
+
+        [TestMethod]
+        public void Deserialize_WithValidDateProvidedForDatumAanvangProperty_SetsPropertyToCorrectValue()
+        {
+            var jsonString = @"{""vestigingsnummer"":""000019061714"",""kvkNummer"":""04005136"",""rsin"":""002932064"",""indNonMailing"":""Nee"",""materieleRegistratie"":{""datumAanvang"":""19290101""},""eersteHandelsnaam"":""Actief B.V."",""indHoofdvestiging"":""Ja"",""indCommercieleVestiging"":""Ja"",""voltijdWerkzamePersonen"":0,""totaalWerkzamePersonen"":0,""deeltijdWerkzamePersonen"":0,""handelsnamen"":[{""naam"":""Actief B.V."",""volgorde"":0}],""adressen"":[{""type"":""bezoekadres"",""indAfgeschermd"":""Nee"",""volledigAdres"":""Industrieweg 9 7944HT Meppel"",""straatnaam"":""Industrieweg"",""huisnummer"":9,""postcode"":""7944HT"",""plaats"":""Meppel"",""land"":""Nederland""}],""websites"":[""www.noordhuis.nl""],""sbiActiviteiten"":[{""sbiCode"":""6810"",""sbiOmschrijving"":""Handel in eigen onroerend goed"",""indHoofdactiviteit"":""Ja""}],""links"":[{""rel"":""self"",""href"":""https://api.kvk.nl/api/v1/vestigingsprofielen/000019061714""},{""rel"":""basisprofiel"",""href"":""https://api.kvk.nl/api/v1/basisprofielen/04005136""}]}";
+
+            var objectGraph = JsonSerializer.Deserialize<Model.Vestiging>(jsonString);
+
+            Assert.AreEqual(new DateTime(year: 1929, month: 1, day: 1), objectGraph.MaterieleRegistratie.DatumAanvang);
+        }
+
+        [TestMethod]
+        public void Deserialize_WithInvalidDateProvidedForDatumAanvangProperty_SetsPropertyToDateTimeMinValue()
+        {
+            var jsonString = @"{""vestigingsnummer"":""000019061714"",""kvkNummer"":""04005136"",""rsin"":""002932064"",""indNonMailing"":""Nee"",""materieleRegistratie"":{""datumAanvang"":""19290000""},""eersteHandelsnaam"":""Actief B.V."",""indHoofdvestiging"":""Ja"",""indCommercieleVestiging"":""Ja"",""voltijdWerkzamePersonen"":0,""totaalWerkzamePersonen"":0,""deeltijdWerkzamePersonen"":0,""handelsnamen"":[{""naam"":""Actief B.V."",""volgorde"":0}],""adressen"":[{""type"":""bezoekadres"",""indAfgeschermd"":""Nee"",""volledigAdres"":""Industrieweg 9 7944HT Meppel"",""straatnaam"":""Industrieweg"",""huisnummer"":9,""postcode"":""7944HT"",""plaats"":""Meppel"",""land"":""Nederland""}],""websites"":[""www.noordhuis.nl""],""sbiActiviteiten"":[{""sbiCode"":""6810"",""sbiOmschrijving"":""Handel in eigen onroerend goed"",""indHoofdactiviteit"":""Ja""}],""links"":[{""rel"":""self"",""href"":""https://api.kvk.nl/api/v1/vestigingsprofielen/000019061714""},{""rel"":""basisprofiel"",""href"":""https://api.kvk.nl/api/v1/basisprofielen/04005136""}]}";
+
+            var objectGraph = JsonSerializer.Deserialize<Model.Vestiging>(jsonString);
+
+            Assert.AreEqual(DateTime.MinValue, objectGraph.MaterieleRegistratie.DatumAanvang);
+        }
+
+        [TestMethod]
+        public void Deserialize_WithNullValueProvidedForDatumAanvangProperty_SetsPropertyToNull()
+        {
+            var jsonString = @"{""vestigingsnummer"":""000019061714"",""kvkNummer"":""04005136"",""rsin"":""002932064"",""indNonMailing"":""Nee"",""materieleRegistratie"":{""datumAanvang"":null},""eersteHandelsnaam"":""Actief B.V."",""indHoofdvestiging"":""Ja"",""indCommercieleVestiging"":""Ja"",""voltijdWerkzamePersonen"":0,""totaalWerkzamePersonen"":0,""deeltijdWerkzamePersonen"":0,""handelsnamen"":[{""naam"":""Actief B.V."",""volgorde"":0}],""adressen"":[{""type"":""bezoekadres"",""indAfgeschermd"":""Nee"",""volledigAdres"":""Industrieweg 9 7944HT Meppel"",""straatnaam"":""Industrieweg"",""huisnummer"":9,""postcode"":""7944HT"",""plaats"":""Meppel"",""land"":""Nederland""}],""websites"":[""www.noordhuis.nl""],""sbiActiviteiten"":[{""sbiCode"":""6810"",""sbiOmschrijving"":""Handel in eigen onroerend goed"",""indHoofdactiviteit"":""Ja""}],""links"":[{""rel"":""self"",""href"":""https://api.kvk.nl/api/v1/vestigingsprofielen/000019061714""},{""rel"":""basisprofiel"",""href"":""https://api.kvk.nl/api/v1/basisprofielen/04005136""}]}";
+
+            var objectGraph = JsonSerializer.Deserialize<Model.Vestiging>(jsonString);
+
+            Assert.IsNull(objectGraph.MaterieleRegistratie.DatumAanvang);
         }
     }
 }
--- HR.KvkConnector/Model/MaterieleRegistratie.cs
+++ HR.KvkConnector/Model/MaterieleRegistratie.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Globalization;
 using System.Runtime.Serialization;
-using System.Text.Json.Serialization;
 
 namespace HR.KvkConnector.Model
 {
@@ -11,18 +11,30 @@ namespace HR.KvkConnector.Model
         /// <summary>
         /// Startdatum onderneming.
         /// </summary>
-        public DateTime? DatumAanvang
+        public DateTime? DatumAanvang { get; set; }
+
+        /// <summary>
+        /// The API seems to be returning invalidly formatted dates for the <see cref="DatumAanvang"/> property from time to time (e.g., "19290000").
+        /// Parsing the date manually in this shadow property to prevent a runtime exception from being thrown.
+        /// </summary>
+        [DataMember(Name = "datumAanvang")]
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        protected string DatumAanvangString
         {
-            get
+            get => DatumAanvang?.ToString("yyyyMMdd");
+            set
             {
-                DateTime.TryParseExact(DatumAanvraagAsText, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out var result);
-                return result;
+                if (string.IsNullOrEmpty(value))
+                {
+                    DatumAanvang = null;
+                }
+                else
+                {
+                    DateTime.TryParseExact(value, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out var result);
+                    DatumAanvang = result;
+                }
             }
         }
-
-
-        [DataMember(Name = "datumAanvang")]
-        public string DatumAanvraagAsText { get; set; }
 
         /// <summary>
         /// Einddatum onderneming.
